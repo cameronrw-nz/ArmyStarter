c58db7bf1dab1de728c4cd81bf64710a4c47239d
Cameron Williams <cameronrw.nz@gmail.com>
2019-06-30 22:37:14 +0700

    Adding Army Pages to Blazor
    Adding Army Pages to blazor including the Army Units.

---
+++ ArmyStarter.Blazor/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArmyStarter.Blazor
+{
+    public static class Constants
+    {
+        public static string RootUrl = "http://localhost:90/api/";
+
+        public static string ArmiesController = RootUrl + "armies";
+
+        public static string ArmyUnitsController = RootUrl + "armyUnits";
+    }
+}
---
+++ ArmyStarter.Blazor/Models/Option.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArmyStarter.Api.Models
+{
+    public class Option
+    {
+        public Guid OptionId { get; set; }
+
+        public string Name { get; set; }
+
+        public int Cost { get; set; }
+
+        public string Link { get; set; }
+    }
+}
