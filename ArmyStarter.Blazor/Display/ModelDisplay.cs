c4aaa0c5b87ec2d6d65c2897d9f18369abde8b0d
Cameron Williams <cameronrw.nz@gmail.com>
2019-07-10 20:22:20 +0700

    Persisting the Number of models between Attack type switching
    Persisting the Number of models between Attack type switching

---
+++ ArmyStarter.Blazor/Display/ModelDisplay.cs
@@ -0,0 +1,38 @@
+﻿using ArmyStarter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArmyStarter.Blazor.Display
+{
+    public class ModelDisplay
+    {
+        private int numberOfModels;
+
+        public ModelDisplay(Model model)
+        {
+            Model = model;
+            Console.WriteLine(NumberOfModels);
+            NumberOfModels = 1;
+
+        }
+
+        public Model Model { get; }
+
+        public int NumberOfModels
+        {
+            get
+            {
+                Console.WriteLine("get number");
+                return numberOfModels;
+            }
+            set
+            {
+                numberOfModels = value;
+                Console.WriteLine("Set number");
+
+            }
+        }
+    }
+}
